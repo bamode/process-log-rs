9cdf4810177c3e265ed6fba6a739d484850659d1
Brent Mode <bmode@wisc.edu>
2022-02-26 20:12:23 -0600

    Refactored the `main` function to be clearer.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 /// Wisconsin IceCube Particle Astrophysics Center
 /// 
 /// Created 25 February 2022
-/// Current version: 0.2.0
+/// Current version: 0.3.0
 /// Status: operative
 
 use std::fs::File;
@@ -17,17 +17,30 @@ fn main() {
     // handle args
     let cli = Cli::parse();
     let filename = cli.file;
-    let mut file = File::open(&filename).unwrap();
-    let mut contents = String::new(); 
-    file.read_to_string(&mut contents).unwrap();
-    let (_, res) = contents.split_once("\n").unwrap(); // remove the header
-    contents = String::from(res);
-    let file_header = filename
+
+    let (ramps, runs, vpeds, bools, file_header) = read_log_file(&filename).unwrap();
+    write_cal_file(&runs, file_header).unwrap();
+    write_tf_files(&ramps, &runs, &vpeds, &bools, file_header).unwrap();
+    println!("exiting...");
+}
+
+fn get_file_header(filename: &str) -> &str {
+    filename
         .split("/") // split on pathing (in *NIX systems)
         .collect::<Vec<&str>>() // collect split into a `Vec` of string slices
         .pop().unwrap() // get just the last element, e.g. the filename
         .split(".") // split the filename from extensions
-        .collect::<Vec<&str>>()[0]; // just keep the important part of the name
+        .collect::<Vec<&str>>()[0] // just keep the important part of the name
+}
+
+fn read_log_file(filename: &str) -> std::io::Result<(Vec<u64>, Vec<String>, Vec<isize>, Vec<bool>, &str)> {
+    println!("reading: {}", filename);
+    let mut file = File::open(&filename)?;
+    let mut contents = String::new(); 
+    file.read_to_string(&mut contents).unwrap();
+    let (_, res) = contents.split_once("\n").unwrap(); // remove the header
+    contents = String::from(res);
+    let file_header = get_file_header(filename);
 
     let (mut ramps, runs, vpeds) = parse_log(&contents); // ramps is mutable because it has duplicates in the logs
     ramps.dedup(); // remove duplicates
@@ -36,9 +49,12 @@ fn main() {
     for i in 0..vpeds.len() - 1 {
         bools.push(vpeds[i + 1] - vpeds[i] > 0); // find the transitions from one isel to another by looking at vpeds
     }
-    bools.push(true); // make the vector lengths equal
+    bools.push(true); // make the vector lengths equal to vpeds.len()
+    Ok((ramps, runs, vpeds, bools, file_header))
+}
 
-    let mut output: String = String::new(); // make a string to buffer the output
+fn write_cal_file(runs: &Vec<String>, file_header: &str) -> std::io::Result<()> {
+    let mut output = String::new(); // make a string to buffer the output
     for i in 0..runs.len() {
         let run = &runs[i]; // needs to be borrowed because a `String` is not `Copy`
         // `push_str` is roughly equivalent in this scenario to `push` on a `Vec`
@@ -46,10 +62,16 @@ fn main() {
     }
     
     // make the output file and unwrap, handling of permissions errors should be supported
-    let mut new_file = File::create(format!("{}-cal-list.txt", file_header)).unwrap();
-    new_file.write_all(output.trim_end().as_bytes()).unwrap(); // have to write output as bytes
-    
-    output = String::new(); // reset our output buffer
+    let new_file_name = format!("{}-cal-list.txt", file_header);
+    println!("writing: {}", new_file_name);
+    let mut new_file = File::create(new_file_name)?;
+    new_file.write_all(output.trim_end().as_bytes())?; // have to write output as bytes
+
+    Ok(())
+}
+
+fn write_tf_files(ramps: &Vec<u64>, runs: &Vec<String>, vpeds: &Vec<isize>, bools: &Vec<bool>, file_header: &str) -> std::io::Result<()> {
+    let mut output = String::new(); // reset our output buffer
     let mut iramp = ramps.iter(); // we want to write out files whenever we change data sets, so we iterate ramps separately
     for i in 0..runs.len() {
         let run = &runs[i];
@@ -58,11 +80,15 @@ fn main() {
         output.push_str(&format!("/data/wipac/CTA/target5and7data/runs_320000_through_329999/cal{}.r1 {}\n", run, vped));
         if !b {
             let ramp = iramp.next().unwrap();
-            let mut new_file = File::create(format!("{}-ramp-{}-tf-dac-list.txt", file_header, ramp)).unwrap();
+            let new_file_name = format!("{}-ramp-{}-tf-dac-list.txt", file_header, ramp);
+            println!("writing: {}", new_file_name);
+            let mut new_file = File::create(new_file_name).unwrap();
             new_file.write_all(output.trim_end().as_bytes()).unwrap();
             output = String::new();
         }
     }
+
+    Ok(())
 }
 
 /// A simple function for parsing the log files that are currently produced in the
